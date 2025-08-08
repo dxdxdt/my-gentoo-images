8c2f8c17ca47d6e8904a34a0edb1e25eb64e72aa
David Timber <dxdt@dev.snart.me>
2025-08-09 01:37:34 +0900

    cloud-openrc-musl ...
    - Add package: app-emulation/qemu-guest-agent
    - Put accept_keywords in the files

--- cloud-openrc-musl/emerge.sh
+++ cloud-openrc-musl/emerge.sh
@@ -4,10 +4,15 @@ set -o pipefail
 
 mkdir -p /boot/efi
 
+echo '# https://bugs.gentoo.org/958594' >> /etc/portage/package.accept_keywords/gentoo-kernel
+echo 'sys-kernel/gentoo-kernel' >> /etc/portage/package.accept_keywords/gentoo-kernel
+echo 'app-emulation/qemu-guest-agent' >> /etc/portage/package.accept_keywords/qemu-guest-agent
+
 emerge-webrsync
 eselect profile set default/linux/$1/23.0/musl
 
 emerge --update --deep --changed-use @world
+. /etc/profile
 emerge --depclean
 
 echo 'sys-kernel/installkernel grub dracut' > /etc/portage/package.use/installkernel
@@ -16,9 +21,8 @@ echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge -qv sys-kernel/installkernel
 echo 'hostonly="no"' >> /etc/dracut.conf
 
-#emerge -qv sys-kernel/linux-firmware
-# https://bugs.gentoo.org/958594
-ACCEPT_KEYWORDS="~$1" emerge -qv '>=sys-kernel/gentoo-kernel-6.12.39'
+#emerge -qv sys-kernel/linux-firmware # licence issues
+emerge -qv '>=sys-kernel/gentoo-kernel-6.12.39'
 
 USE=netifrc emerge -qv app-emulation/cloud-init
 echo 'locale: false' > /etc/cloud/cloud.cfg.d/99-musl-disable-locale.cfg
@@ -34,6 +38,9 @@ emerge -qv \
 	sys-process/cronie app-shells/bash-completion net-misc/chrony \
 	sys-fs/dosfstools sys-fs/e2fsprogs sys-fs/xfsprogs \
 	sys-block/io-scheduler-udev-rules sys-fs/growpart
+# For KVM based VPS providers. The service has to be added manually as part of
+# the integration/image building process.
+emerge -qv app-emulation/qemu-guest-agent
 
 rc-update add sshd
 rc-update add cronie
